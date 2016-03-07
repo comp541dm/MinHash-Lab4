87e9fb35960105bd78bf09332d1a712d7559eb66
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2016-03-06 18:23:11 -0800

    Got row versions working.

---
+++ mapperRow.java
@@ -0,0 +1,120 @@
+import java.lang.*;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Arrays;
+
+public class mapperRow {
+
+  public static int numColumns;
+  public static int numRows;
+  public static int numHashes; 
+  public static int a[];
+  public static int b[];
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    numHashes = Integer.parseInt(args[0]);
+    numRows = Integer.parseInt(args[1]);
+    numColumns = Integer.parseInt(args[2]);
+    a = new int[numHashes];
+    b = new int[numHashes];
+    int[][] signatureMatrix = new int[numHashes][numColumns];
+    // Need some way to generate an array of hashes
+    // hash hashes = new hash[numHashes];
+    // Uses these combined with hash function for multiple random hashes
+    Random rand = new Random();
+
+    // Initialize values in signatureMatrix to MAX
+    // and random numbers to a & b
+    for (int i = 0; i < numHashes; i++) {
+      a[i] = Math.abs(rand.nextInt());
+      b[i] = Math.abs(rand.nextInt());
+      // System.err.println("a[" + i + "]: " + a[i] + " b[" + i + "]: " + b[i]);
+      for (int j = 0; j < numColumns; j++) {
+        signatureMatrix[i][j] = Integer.MAX_VALUE;
+      }
+    }
+
+    // mapper(signatureMatrix);
+    // Read matrix and pass it into appropriate functions
+    int[] row = new int[numColumns];
+    for (int i = 0; i < numRows; i++) {
+      for (int j = 0; j < numColumns; j++) {
+        row[j] = in.nextInt();
+      }
+      mapperRow(row, i);
+    }
+  }
+
+  public static int hash (int a, int b, int x) {
+    // Uses this formula: h(x, a, b) = ((ax + b) mod p) mod m
+    // x = key a = (1, p-1) b = (0,p-1)
+    // p = prime > max of x = # of rows
+    // m = max possible value + 1 = numHashes
+    int p = numRows + 1;
+    int m = numHashes;
+    a = (a % p-1) + 1;
+    b = (b % p);
+    return ((a * x + b) % p) % m;
+  }
+
+  public static void mapper (int[][] signatureMatrix) {
+    Scanner in = new Scanner(System.in);
+    for (int curRow = 0; curRow < numRows; curRow++) {
+      for (int colNum = 0; colNum < numColumns; colNum++) {
+        if (in.nextInt() == 1) {
+          for (int curHash = 0; curHash < numHashes; curHash++) {
+            if (hash(a[curHash], b[curHash], curRow) < signatureMatrix[curHash][colNum]) {
+              signatureMatrix[curHash][colNum] = hash(a[curHash], b[curHash], curRow);
+            } 
+          }
+        }
+      }
+    }
+  } 
+
+
+  public static void mapperColumn (int[] column, int columnIndex) {
+    int[] sigColumn = new int[numHashes];
+    Arrays.fill(sigColumn, Integer.MAX_VALUE);
+    for (int i = 0; i < column.length; i++) {
+      if (column[i] == 1) {
+        for (int j = 0; j < numHashes; j++) {
+          if (hash(a[j], b[j], i) < sigColumn[j]) {
+            sigColumn[j] = hash(a[j], b[j], i);
+          }       
+        }
+      }
+    }
+    System.out.print(columnIndex + ", ");
+    printIntArray(sigColumn);
+  }
+
+  public static void mapperRow (int[] row, int rowIndex) {
+    for (int i = 0; i < numHashes; i++) {
+      System.out.print(i + ", ");
+      for (int j = 0; j < row.length; j++) {
+        if (row[j] == 1) {
+          System.out.print(hash(a[i], b[i], rowIndex) + " ");
+        } else {
+          System.out.print(Integer.MAX_VALUE + " ");
+        }
+      }
+      System.out.println();
+    }
+  }
+
+  public static int[] getColumn (int[][] matrix, int index, int columnLength) {
+    int[] col = new int[columnLength];
+    for (int i = 0; i < columnLength; i++) {
+      col[i] = matrix[i][index];
+    }
+    return col;
+  }
+
+  public static void printIntArray (int[] arr) {
+    for (int i = 0; i < arr.length - 1; i++)
+      System.out.print(arr[i] + " ");
+    System.out.println(arr[arr.length-1]);
+  }
+} 
--- reducerRow.java
+++ reducerRow.java
@@ -2,8 +2,9 @@ import java.lang.*;
 import java.util.Scanner;
 import java.util.Random;
 import java.util.StringTokenizer;
+import java.util.Arrays;
 
-public class reducerColumn {
+public class reducerRow {
 
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
@@ -11,18 +12,33 @@ public class reducerColumn {
     int numColumns = Integer.parseInt(args[1]);
     int[][] signatureMatrix = new int[numHashes][numColumns];
 
+    for (int i = 0; i < numHashes; i++) {
+      Arrays.fill(signatureMatrix[i], Integer.MAX_VALUE);
+    }
+
     while (in.hasNextLine()) {
       String[] line = in.nextLine().split(","); 
-      int column = Integer.parseInt(line[0]);
+      int hashRow = Integer.parseInt(line[0]);
       StringTokenizer st = new StringTokenizer(line[1]);
-      for (int row = 0; st.hasMoreTokens(); row++) {
-        signatureMatrix[row][column] = Integer.parseInt(st.nextToken());
+      for (int column = 0; st.hasMoreTokens(); column++) {
+        int currentHashValue = Integer.parseInt(st.nextToken());
+        if (currentHashValue < signatureMatrix[hashRow][column]) {
+          signatureMatrix[hashRow][column] = currentHashValue;
+        }
       }
     } 
 
     System.out.println("Similarity: " +  similarity(
       getColumn(signatureMatrix, 1, numHashes),
       getColumn(signatureMatrix, 2, numHashes)));
+  }
+ 
+  public static int[] getColumn (int[][] matrix, int index, int columnLength) {
+    int[] col = new int[columnLength];
+    for (int i = 0; i < columnLength; i++) {
+      col[i] = matrix[i][index];
+    }
+    return col;
   }
 
   public static double similarity(int[] s1, int[] s2) {
@@ -34,4 +50,5 @@ public class reducerColumn {
     }
     return sim/(double)s1.length;
   }
+
 } 
